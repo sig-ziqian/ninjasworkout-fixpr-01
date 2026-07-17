5c354e9b760f539945bc357cb43cbf0c13d90c84
sig-ziqian <167462212+sig-ziqian@users.noreply.github.com>
2026-07-17 16:58:48 -0600

    update file routers/index.js

--- routers/index.js
+++ routers/index.js
@@ -652,17 +652,20 @@ var mime = 'html';
   console.log('New request: '+request.url);
 
   needle.get(url, { timeout: 3000 }, function(error, response1) {
+    const safeUrl = String(url).replace(/[&<>"']/g, function (ch) {
+      return ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;' })[ch];
+    });
     if (!error && response1.statusCode == 200) {
       response.writeHead(200, {'Content-Type': 'text/'+mime});
       response.write('<h1>Welcome to Ninzas\'s SSRF demo.</h1>\n\n');
-      response.write('<h2>I am an application. I want to be useful, so I requested: <font color="red">'+url+'</font> for you\n</h2><br><br>\n\n\n');
+      response.write('<h2>I am an application. I want to be useful, so I requested: <font color="red">'+safeUrl+'</font> for you\n</h2><br><br>\n\n\n');
       console.log(response1.body);
       response.write(response1.body);
       response.end();
     } else {
       response.writeHead(404, {'Content-Type': 'text/'+mime});
       response.write('<h1>Welcome to Ninzas\'s SSRF demo.</h1>\n\n');
-      response.write('<h2>I wanted to be useful, but I could not find: <font color="red">'+url+'</font> for you\n</h2><br><br>\n\n\n');
+      response.write('<h2>I wanted to be useful, but I could not find: <font color="red">'+safeUrl+'</font> for you\n</h2><br><br>\n\n\n');
       response.end();
       console.log('error')
 
